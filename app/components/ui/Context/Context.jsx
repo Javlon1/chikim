6f71cdba55515098b42d26b8d637bea1f6973c7f
Javlon1 <muhamedmijotot@gmail.com>
2023-12-28 14:32:44 +0500

    General

--- app/components/ui/Context/Context.jsx
+++ app/components/ui/Context/Context.jsx
@@ -2,7 +2,8 @@ import { createContext, useState } from 'react'
 const Context = createContext()
 
 function Provider({ children }) {
-    const [url] = useState("http://localhost:3000/api")
+    const [url] = useState("https://chikim.vercel.app/api")
+    // const [url] = useState("http://localhost:3000/api")
 
     return (
         <Context.Provider value={{ url }}>
