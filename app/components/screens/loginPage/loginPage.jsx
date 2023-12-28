eb4ba39323175d93224efe2d3da4d7f350989e01
Javlon1 <muhamedmijotot@gmail.com>
2023-12-28 11:38:42 +0500

    name

--- app/components/screens/loginPage/loginPage.jsx
+++ app/components/screens/loginPage/loginPage.jsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import Link from 'next/link'
 import Image from 'next/image'
 import styles from './LoginPage.module.scss'
-import { Context } from '@/app/components/ui/Context/Context';
 import MyContainer from '@/app/components/ui/MyContainer/MyContainer'
 import loginImg from "../../../../public/image/login.svg";
 import { useRouter } from 'next/router';
@@ -10,7 +8,7 @@ import { useRouter } from 'next/router';
 const LoginPage = () => {
     const router = useRouter();
 
-    const Submit = (e) => {
+    const Submit = () => {
         router.push('/');
     }
     return (
