1b2005a99b70ad341cd6e502e126dcbb4f51ee81
Javlon1 <muhamedmijotot@gmail.com>
2023-12-30 03:02:44 +0500

    General Page end

--- app/components/screens/general/GeneralPage.jsx
+++ app/components/screens/general/GeneralPage.jsx
@@ -17,6 +17,13 @@ const GeneralPage = ({ initialChecked = false }) => {
     const [monthData, setMonthData] = React.useState();
     // const allEmojis = ['👕', '👔', '👚', '👗', '👙', '👘', '👠', '👡', '👢', '👞', '🧦', '🧤', '🧣', '🎩', '🧢', '👒', '🎓', '🧢', '👑', '🎒', '👝', '👛', '👜', '🕶️', '🌂', '🥤', '🍹', '🍸', '🍷', '🥂', '🍺', '🍻', '🥃', '🍶', '☕', '🍵', '🧉', '🚖', '🚕', '🚗', '🚘', '🚙', '🛺', '🚍', '🚌', '🚎', '🚐', '🚚', '🚛', '🚜', '🛴', '🚲', '🛵', '🏍️', '🍔', '🍕', '🍟', '🌭', '🍿', '🍱', '🍲', '🍜', '🍛', '🍝', '🍠', '🍤', '🍥', '🍣', '🍙', '🍚', '🍘', '🍢', '🍡', '🍦', '🍧', '🍨', '🍩', '🍪', '🎂', '🍰', '🧁', '🥧', '🍫', '🍬', '🍭', '🍮', '🍯', '🍎', '🍏', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🍈', '🍒', '🍑', '🥭', '🍍', '🥥', '🥝'];
     const [emoji, setEmoji] = React.useState('👕');
+    const [totalPrice, setTotalPrice] = React.useState(545)
+    const [chartData, setChartData] = React.useState([
+        { emoji: "👕", price: 50 },
+        { emoji: "☕", price: 60 },
+        { emoji: "🚕", price: 50 },
+        { emoji: "🍰", price: 70 },
+    ]);
 
     const endpointGet = 'month';
 
@@ -42,6 +49,8 @@ const GeneralPage = ({ initialChecked = false }) => {
         return date.getDate();
     };
 
+    const daysInSelectedMonth = daysInMonth(selectedYear, selectedMonth);
+
     const handleDayChange = (day) => {
         setSelectedDay(day);
     };
@@ -50,41 +59,18 @@ const GeneralPage = ({ initialChecked = false }) => {
         setSelectedMonth(month);
     };
 
-    const daysInSelectedMonth = daysInMonth(selectedYear, selectedMonth);
-
-
-    const [totalPrice, setTotalPrice] = React.useState(445)
-
-    const myData = [
-        {
-            price: 50,
-        },
-        {
-            price: 60,
-        },
-        {
-            price: 30,
-        },
-        {
-            price: 40,
-        },
-        {
-            price: 55,
-        },
-        {
-            price: 90,
-        },
-    ]
-
-
     const calculateWidth = (e) => {
         const totalPercentage = (e / totalPrice) * 100;
         return totalPercentage > 100 ? '100%' : `${totalPercentage}%`;
     };
 
-
-    const totalSum = myData.reduce((sum, item) => sum + item.price, 0);
+    const totalSum = chartData.reduce((sum, item) => sum + item.price, 0);
     const redColor = totalSum >= totalPrice ? "red" : ""
+
+    const chartDataWithPercentage = chartData.map(item => ({
+        ...item,
+        percentage: (item.price / totalSum) * 100,
+    }));
 
     return (
         <section className={styles.generalPage}>
@@ -186,7 +172,7 @@ const GeneralPage = ({ initialChecked = false }) => {
                         </ul>
                         <ul className={styles.generalPage__items__used__list2}>
                             {
-                                myData?.map((e, i) => (
+                                chartData?.map((e, i) => (
                                     <li
                                         key={i}
                                         className={styles.generalPage__items__used__list2__item}
@@ -203,28 +189,16 @@ const GeneralPage = ({ initialChecked = false }) => {
                     <div className={styles.generalPage__items__chart}>
                         <p className={styles.generalPage__items__chart__title}>Turkumlar ko’rinishida</p>
                         <div className={styles.generalPage__items__chart__item}>
-                            <PieChart price={totalSum} data={myData} />
+                            <PieChart price={totalSum} data={chartData} />
                             <ul className={styles.generalPage__items__chart__item__list}>
-                                <li className={styles.generalPage__items__chart__item__list__item}>
-                                    <p>{emoji}</p>
-                                    <p>10%</p>
-                                </li>
-                                <li className={styles.generalPage__items__chart__item__list__item}>
-                                    <p>{emoji}</p>
-                                    <p>10%</p>
-                                </li>
-                                <li className={styles.generalPage__items__chart__item__list__item}>
-                                    <p>{emoji}</p>
-                                    <p>10%</p>
-                                </li>
-                                <li className={styles.generalPage__items__chart__item__list__item}>
-                                    <p>{emoji}</p>
-                                    <p>10%</p>
-                                </li>
-                                <li className={styles.generalPage__items__chart__item__list__item}>
-                                    <p>{emoji}</p>
-                                    <p>10%</p>
-                                </li>
+                                {chartDataWithPercentage.map((item, index) => (
+                                    <li className={styles.generalPage__items__chart__item__list__item} key={index}>
+                                        <p>{item.emoji}</p>
+                                        <p>
+                                            {`${item.percentage.toFixed(2)}%`}
+                                        </p>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
