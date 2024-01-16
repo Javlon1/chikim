d905b747a2da1a133e18a3c55dc9b6a0e4e243c6
Javlon1 <muhamedmijotot@gmail.com>
2024-01-16 14:46:30 +0500

    setting V-3

--- app/components/screens/SettingPage/SettingPage.jsx
+++ app/components/screens/SettingPage/SettingPage.jsx
@@ -273,7 +273,10 @@ const SettingPage = () => {
                             />
 
                             <b onClick={togglePicker}>
-                                <p>+</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+                                    <rect x="20" y="45" width="60" height="10" fill="black" />
+                                    <rect x="45" y="20" width="10" height="60" fill="black" />
+                                </svg>
                             </b>
                         </div>
                         {showPicker && (
